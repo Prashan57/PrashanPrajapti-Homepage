a59514f1a6fd6f906bc73e29a4e81005fb7bb6f0
Prashan <officialprashan@gmail.com>
2024-08-08 02:14:27 +0545

    tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -68,7 +68,7 @@ const config = {
           to: { height: "0" },
         },
         meteor: {
-          "0%": { transform: "rotate(215deg) translateX(0)", opacity: 1 },
+          "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
           "70%": { opacity: "1" },
           "100%": {
             transform: "rotate(215deg) translateX(-500px)",
